8750a237176e9817a6fd7d3817f32b203f9bc983
lebob18-ux <lebob18@gmail.com>
2026-07-14 22:16:19 +0200

    Rename canton variable to 'Canton_Nord'

--- CANTON.js
+++ CANTON.js
@@ -1,5 +1,5 @@
 // Nom du canton
-const nomCanton = "746000bob";
+const nomCanton = "Canton_Nord";
 // Données du canton : liste des supports avec leur identifiant et PK
 const donneesCanton = [
     {identifiant: "045/30", pointKilometrique: 45.500}, {identifiant: "045/34", pointKilometrique: 45.608},
